cc9cd112e13f642f4505b4c4cb2f54327f85fa0d
Simon Glancy <simonglancy@Simons-MacBook-Pro.local>
2016-03-01 18:43:11 +0000

    added TODAY to code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,13 @@ attr_reader :single_bike
   end
 
   def release_bike
-    raise 'no more bikes left' if @single_bike == nil
+    raise 'no more bikes left TODAY' unless @single_bike
     out_bike, @single_bike = @single_bike, nil
     out_bike
   end
 
   def return_bike(returned_bike)
-    raise 'storage full - TOO MANY BIKES'if @single_bike != nil
+    raise 'storage full - TOO MANY BIKES'if @single_bike
     @single_bike = returned_bike
   end
 
