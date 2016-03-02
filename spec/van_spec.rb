24fec72ec21a06cb5174e13d1c0232ab96e1cec0
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-02 17:26:42 +0000

    Added test for garage taking bikes plus added ability for van and garage to take bikes

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -8,7 +8,7 @@ describe Van do
       broken_bike = double(:bike, broken: true)
       bike = double(:bike, broken: false)
       subject.take_bikes([bike, broken_bike],true)
-      expect(subject.bikes).to eq([broken_bike]) 
+      expect(subject.bikes).to eq([broken_bike])
     end
 
   end
