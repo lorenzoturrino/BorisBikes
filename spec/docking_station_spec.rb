536b3e66404aee86df0b8547d2ca574535cccd99
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-02 16:05:37 +0000

    Added doubles for bike in docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,18 @@ require 'docking_station'
 
 describe DockingStation do
 
-  describe 'release_bike' do
+  let(:bike) {double :bike}
 
-    it 'releases a bike' do
-      subject.dock(Bike.new)
-      expect(subject.release_bike).to be_a Bike
-    end
+  describe 'release_bike' do
 
     it 'raises exception when no bikes to release' do
       expect{subject.release_bike}.to raise_error 'No bikes to release' if subject.bikes.empty?
     end
 
-  end
-
-  describe 'working?' do
-
-    it 'bike works' do
-      subject.dock(Bike.new)
+    it 'expects bike to be released and bike to work' do
+      allow(bike).to receive(:working?).and_return(true)
+      allow(bike).to receive(:broken).and_return(false)
+      subject.dock bike
       expect(subject.release_bike).to be_working
     end
 
@@ -26,26 +21,26 @@ describe DockingStation do
 
   describe 'dock' do
 
+
     it 'docks given bike' do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.bikes).to eq([bike])
     end
 
     it 'raises an exception when there is already a bike docked' do
-      subject.capacity.times{subject.dock(Bike.new)}
-      expect{subject.dock(Bike.new)}.to raise_error 'Docking Station full'
+      subject.capacity.times{subject.dock(bike)}
+      expect{subject.dock(bike)}.to raise_error 'Docking Station full'
     end
 
     it 'does not release broken bike' do
-      bike = Bike.new
-      bike.report_broken
+      allow(bike).to receive(:broken).and_return(true)
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error 'No bikes to release'
     end
 
     it 'accepts all bikes' do
-      bike = Bike.new.report_broken
+      allow(bike).to receive(:report_broken).and_return(true)
+      bike.report_broken
       subject.dock(bike)
       expect(subject.bikes).to include(bike)
     end
