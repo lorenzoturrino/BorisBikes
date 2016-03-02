0ab39bcb54ebecae0974b89c58695e41195103b6
Lorenzo Turrino <lorenzoturrino@gmail.com>
2016-03-02 11:00:48 +0000

    create rspec test for full rack

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
-    it { is_expected.to respond_to :release_bike }
-
-    describe '#release_bike' do
+    describe '#release_bike:' do
+      it { is_expected.to respond_to :release_bike }
       it 'releases a bike' do
         bike = Bike.new
         subject.dock(bike)
@@ -16,14 +15,22 @@ describe DockingStation do
       end
     end
 
-    it { is_expected.to respond_to(:dock).with(1).argument }
+    describe '#dock:' do
+      it { is_expected.to respond_to(:dock).with(1).argument }
+      it 'docks something' do
+      	bike = Bike.new
+      	expect(subject.dock(bike)).to eq bike
+      end
+      it 'dock method raises an error when trying to dock to a full rack' do
+        expect {
+          while true do
+            subject.dock(Bike.new)
+          end
+        }.to raise_error("Station is full")
+      end
+    end
 
     it { is_expected.to respond_to(:bike) }
-
-    it 'docks something' do
-    	bike = Bike.new
-    	expect(subject.dock(bike)).to eq bike
-    end
 
     it 'returns docked bikes' do
    		bike = Bike.new
