cae7f0c4cf2b01f78dcecefaba5f2060ab16cc8f
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-02 11:43:21 +0000

    refactor for SRP principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,23 @@ class DockingStation
 	end
 
   def release_bike
-    fail 'No bikes available' if @bike_array.empty?
+    fail 'No bikes available' if empty?
     @bike_array.pop
   end
 
   def dock(bike)
-  	fail 'Station is full' if @bike_array.length >= 20
+  	fail 'Station is full' if full?
   	@bike_array << bike
   end
+
+	private
+
+	def full?
+		@bike_array.length >= 20
+	end
+
+	def empty?
+		@bike_array.empty?
+	end
 
 end
